a0a6d2b00587a4513b30afaac75755c1e04d456d
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2024-04-29 14:18:30 -0400

    Corriger Erreurs

--- tasks/main_pratique.rb
+++ tasks/main_pratique.rb
@@ -4,19 +4,19 @@
 
 $graphical = true
 
-require_relative "pratique"
 require_relative "../karel/robota"
+require_relative "pratique"
 
 # a task for a stair sweeper
 def task()
-  
   karel = Pratique.new(1, 1, Robota::EAST, 50)
-  karel.move_5
-  karel.mettre_5_beepers
-  karel.turn_around
-  karel.diagonale_droit
-  karel.put_beeeper
-  karel.turn_off
+  karel.move_5()
+  karel.mettre_5_beepers()
+  karel.turn_around()
+  karel.turn_left() # Added
+  karel.diagonale_droit()
+  karel.put_beeper()
+  karel.turn_off()
   
 end
 
--- tasks/pratique.rb
+++ tasks/pratique.rb
@@ -1,37 +1,43 @@
-require_relative "../karel/ur_robot"
+require_relative("../karel/ur_robot")
+require_relative("../mixins/turner.rb")
 
 class Pratique < UrRobot
+  include Turner  
+  def move_5()
+	  move()
+	  move()
+	  move()
+	  move()
+	  move()
+  end 
 
-	def move_5
-		move
-		move
-		mov
-		move
-		move
-	end 
+  def mettre_5_beepers()
+	  put_beeper()
+	  put_beeper()
+	  put_beeper()
+	  put_beeper()
+	  put_beeper()
+  end
 
-	def mettre_5_beeper
-		put_beeper
-		put_beeper
-		put_beeper
-		put_beeper
-		put_beeper
-	end
+  def diagonale_droit()
+	  move()
+	  turn_right()
+	  move()
+	  turn_left()
+  end
 
-	def diagonale_droit
-		move
-		turn_right
-		move
-		turn_left
-	end
+  def diagonale_gauche()
+	  move()
+	  turn_left()
+	  move()
+	  turn_right()
+  end
 
-	def diagonale_gauche
-		move
-		turn_left
-		move
-		turn_right		
-	end
-
-
+  def turn_around()
+  	turn_left()
+  	turn_left()
+  	turn_left()
+  	turn_left()
+  end
 end
 
